515fa965d33211a1c000b8f8184dd2f4afc5897c
leeeharam <73048283+leeeharam@users.noreply.github.com>
2021-01-07 14:12:42 +0900

    Add files via upload

---
+++ upload.js
@@ -0,0 +1,57 @@
+// ============================================== //
+
+// al 0002 까지
+
+let al_0001_title = "Kotlin으로 Stack 구현하기"
+let al_0001_date = "2020. 10. 10"
+
+let al_0002_title = "Kotlin으로 Queue 구현하기"
+let al_0002_date = "2020. 10. 11"
+
+// boj 0002 까지
+
+let boj_0001_title = "17293 - 맥주 99병 - Kotlin"
+let boj_0001_date = "2020. 09. 08"
+
+let boj_0002_title = "10801 - 카드게임 - Kotlin"
+let boj_0002_date = "2020. 09. 08"
+
+
+// i 0001 까지
+
+let i_0001_title = "Swift에 대해 알아보자"
+let i_0001_date = "2021. 01. 01"
+
+// front 0000 까지
+
+let front_0001_title = "CSS 동영상 첨부 코드 목록"
+let front_0001_date = "2021. 07. 02"
+
+let front_0002_title = "HTML / CSS / JS만 가지고 Github 블로그 만들기"
+let front_0002_date = "2021. 01. 02"
+
+// com 0001 까지
+
+let com_0001_title = "CPU와 디지털 회로"
+let com_0001_date = "2020. 10. 17"
+
+// com 0001 까지
+
+// ============================================= //
+
+let al_new_title = al_0001_title
+let al_new_date = al_0001_date
+
+let boj_new_title = boj_0001_title
+let boj_new_date = boj_0001_date
+
+let i_new_title = i_0001_title
+let i_new_date = i_0001_date
+
+let front_new_title = front_0001_title
+let front_new_date = front_0001_date
+
+let com_new_title = com_0001_title
+let com_new_date = com_0001_date
+
+// ============================================== //
